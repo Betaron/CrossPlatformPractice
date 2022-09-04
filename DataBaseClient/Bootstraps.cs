2e248f080b0bea224fb498a77dffafb678744c84
Иван Агабалаев <agabalaev01@yandex.ru>
2022-09-04 20:06:44 +0300

    Add an extension method to add Dependency Injections
    The Bootstraps class adds dependencies to MauiProgram
    they are added to the builder.

---
+++ DataBaseClient/Bootstraps.cs
@@ -0,0 +1,14 @@
+﻿using DataBaseClient.Gateways.Users;
+using DataBaseClient.Gateways.Users.Repositories;
+
+namespace DataBaseClient;
+
+internal static class Bootstraps
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
