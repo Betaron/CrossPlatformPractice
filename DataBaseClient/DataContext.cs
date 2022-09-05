2da2538cb3e989137cb8c5915c2512a0a63a3dc6
Иван Агабалаев <agabalaev01@yandex.ru>
2022-09-05 16:41:08 +0300

    Add a class representing the context for interacting with the data
    The class contains a property that stores data about users.

---
+++ DataBaseClient/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace DataBaseClient
+{
+    internal class DataContext
+    {
+        public Hashtable Users { get; private set; }
+
+        DataContext()
+        {
+            Users = new Hashtable();
+        }
+    }
+}
