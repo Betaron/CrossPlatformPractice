427cacbc0cf15dd8159c86d9f8b9e7bcbea6f8f1
Иван Агабалаев <agabalaev01@yandex.ru>
2022-09-07 13:01:52 +0300

    Change the namespace setting
    change
    namespace smth { ... }
    on the
    namespace;

--- DataBaseClient/DataContext.cs
+++ DataBaseClient/DataContext.cs
@@ -1,15 +1,14 @@
 ﻿using DataBaseClient.Models;
 using System.Collections;
 
-namespace DataBaseClient
-{
-    internal class DataContext
-    {
-        public Dictionary<Guid, User> Users { get; private set; }
+namespace DataBaseClient;
 
-        DataContext()
-        {
-            Users = new Dictionary<Guid, User>();
-        }
+internal class DataContext
+{
+    public Dictionary<Guid, User> Users { get; private set; }
+
+    DataContext()
+    {
+        Users = new Dictionary<Guid, User>();
     }
 }
